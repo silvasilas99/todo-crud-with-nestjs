550ad929f546220286644c8ab6a85ac5287f3d48
Silas Silva <silvasilas66@gmail.com>
2021-05-19 23:33:16 -0300

    First commit

---
+++ src/shared/services/database-connection.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common'
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm'
+import 'dotenv/config'
+
+@Injectable()
+export class DatabaseConnectionService implements TypeOrmOptionsFactory {
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      name: 'default',
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DB,
+      synchronize: true,
+      dropSchema: false,
+      logging: true,
+      entities: ['dist/**/*.entity.js'],
+    }
+  }
+}
